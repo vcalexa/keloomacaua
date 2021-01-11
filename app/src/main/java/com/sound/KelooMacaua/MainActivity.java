0ff9369cee16c01594d17de4545d9f0ceafad92e
Claudiu Haidu <claudiu.haidu@p3-holding.com>
2021-01-11 18:05:56 +0200

    simple image swap and hide/remove

--- app/src/main/java/com/sound/KelooMacaua/MainActivity.java
+++ app/src/main/java/com/sound/KelooMacaua/MainActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView player2Cards;
+    ImageView tablePile;
 
 
     public void calculate(View view){
@@ -20,5 +22,17 @@ public class MainActivity extends AppCompatActivity {
         CardMoves cardMoves = CardMoves.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        player2Cards = findViewById(R.id.player2Cards);
+        tablePile = findViewById(R.id.tablePile);
+
+        player2Cards.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tablePile.setImageResource(R.drawable.ace_of_clubs);
+                player2Cards.setVisibility(View.GONE);
+            }
+        });
+
     }
 }
