5fc0368d5e2a7953c85d60e75fcbceb05f12b582
Claudiu Haidu <claudiu.haidu@p3-holding.com>
2021-01-11 19:19:58 +0200

    deal cards player 2

--- app/src/main/java/com/sound/KelooMacaua/MainActivity.java
+++ app/src/main/java/com/sound/KelooMacaua/MainActivity.java
@@ -6,13 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     ImageView player2Cards;
     ImageView tablePile;
 
 
-    public void calculate(View view){
+    public void calculate(View view) {
         //EditText editText = (EditText) findViewById(R.id.editTextNumber);
         //Integer inputNo = Integer.parseInt(editText.getText().toString());
     }
@@ -23,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        player2Cards = findViewById(R.id.player2Cards);
+        /*player2Cards = findViewById(R.id.player2Cards);
         tablePile = findViewById(R.id.tablePile);
 
         player2Cards.setOnClickListener(new View.OnClickListener() {
@@ -32,7 +35,25 @@ public class MainActivity extends AppCompatActivity {
                 tablePile.setImageResource(R.drawable.ace_of_clubs);
                 player2Cards.setVisibility(View.GONE);
             }
-        });
+        });*/
+
+        Random r = new Random();
+
+        LinearLayout layout = findViewById(R.id.imageLayout);
+        for (int i = 0; i < 10; i++) {
+            ImageView image = new ImageView(this);
+            image.setLayoutParams(new android.view.ViewGroup.LayoutParams(100, 100));
+            image.setMaxHeight(170);
+            image.setMaxWidth(170);
+
+            int n = r.nextInt(53) + 1;
+            Card card = new Card();
+            int imageId = getResources().getIdentifier(String.format("%s_of_%s", card.getCardRank(n), card.getCardSuite(n)).toLowerCase(),
+                    "drawable", getPackageName());
+            image.setImageResource(imageId);
+            // Adds the view to the layout
+            layout.addView(image);
+        }
 
     }
 }
