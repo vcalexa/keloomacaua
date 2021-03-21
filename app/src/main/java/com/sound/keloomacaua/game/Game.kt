e10af53ae6e6205a6065bbc5f295473d774b7bbb
Mircea Nistor <mirceanis@gmail.com>
2021-03-21 11:24:32 +0100

    refactor: convert Game.java to Game.kt

--- app/src/main/java/com/sound/keloomacaua/game/Game.kt
+++ app/src/main/java/com/sound/keloomacaua/game/Game.kt
@@ -1,103 +1,15 @@
-package com.sound.keloomacaua.game;
+package com.sound.keloomacaua.game
 
-import java.io.Serializable;
-import java.util.Date;
-import java.util.List;
+import java.io.Serializable
 
-public class Game implements Serializable {
-    private Date dateTime;
-    private long gameId;
-    private String player1Joined;
-    private String player2Joined;
-    private int playersTurn;
-    private List<Integer> player1Cards;
-    private List<Integer> player2Cards;
-    private List<Integer> deckRemainingCards;
-    private List<Integer> playedCards;
-    private int whoWon;
-
-    public Game() {
-    }
-
-    public Date getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(Date dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public long getGameId() {
-        return gameId;
-    }
-
-    public void setGameId(long gameId) {
-        this.gameId = gameId;
-    }
-
-    public String getPlayer1Joined() {
-        return player1Joined;
-    }
-
-    public void setPlayer1Joined(String player1Joined) {
-        this.player1Joined = player1Joined;
-    }
-
-    public String getPlayer2Joined() {
-        return player2Joined;
-    }
-
-    public void setPlayer2Joined(String player2Joined) {
-        this.player2Joined = player2Joined;
-    }
-
-    public List<Integer> getPlayer1Cards() {
-        return player1Cards;
-    }
-
-    public void setPlayer1Cards(List<Integer> player1Cards) {
-        this.player1Cards = player1Cards;
-    }
-
-    public List<Integer> getPlayer2Cards() {
-        return player2Cards;
-    }
-
-    public void setPlayer2Cards(List<Integer> player2Cards) {
-        this.player2Cards = player2Cards;
-    }
-
-    public List<Integer> getDeckRemainingCards() {
-        return deckRemainingCards;
-    }
-
-    public void setDeckRemainingCards(List<Integer> deckRemainingCards) {
-        this.deckRemainingCards = deckRemainingCards;
-    }
-
-    public List<Integer> getPlayedCards() {
-        return playedCards;
-    }
-
-    public void setPlayedCards(List<Integer> playedCards) {
-        this.playedCards = playedCards;
-    }
-
-    public int getWhoWon() {
-        return whoWon;
-    }
-
-    public void setWhoWon(int whoWon) {
-        this.whoWon = whoWon;
-    }
-
-    public int getPlayersTurn() {
-        return playersTurn;
-    }
-
-    public void setPlayersTurn(int playersTurn) {
-        this.playersTurn = playersTurn;
-    }
-
-
+class Game : Serializable {
+    var gameId: Long = System.currentTimeMillis()
+    var player1Joined: String? = null
+    var player2Joined: String? = null
+    var playersTurn = 0
+    var player1Cards: List<Int>? = null
+    var player2Cards: List<Int>? = null
+    var deckRemainingCards: List<Int>? = null
+    var playedCards: List<Int>? = null
+    var whoWon = -1
 }
